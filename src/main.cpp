9bd188d7ea6660e6ebbb25c37b56d290722ef5e1
Arnau25 <81559427+Arnau25@users.noreply.github.com>
2021-06-17 12:27:33 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+Serial.begin(9600);
+Serial2.begin(9600);
+  Serial.write("Inicializando...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if(Serial.available()){
+    Serial2.write(Serial.read());
+    delay(10);
+    if(Serial2.available()){
+      Serial.write(Serial2.read());
+    }
+  }
 }
